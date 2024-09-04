bd2ae0eb9d096e22621552033cdf475eb59e115c
lntechfuse <161242106+lntechfuse@users.noreply.github.com>
2024-09-04 18:17:20 +0700

    Update page.js

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,9 +1,9 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
 
-export default function SearchResultPage() {
+export default function SearchResultPage({params}) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
 
@@ -17,14 +17,32 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+  const searchInput = params.searchInput;
+  
+  const processedSearchInput = searchInput.replaceAll("%20"," ");
+
+  const filteredMovies = movieDB.filter((movie) => 
+    movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase())
+  );
+
+  console.log(filteredMovies[0].id)
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
-      {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
+      {filteredMovies.map((movie, i) => (
+          <MovieRow
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            detail={movie.detail}
+            rating={movie.rating}
+            number={i + 1}
+          />
+        ))}
     </div>
   );
 }
