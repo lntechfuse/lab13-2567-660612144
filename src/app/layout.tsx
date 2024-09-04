07d113c6bc0e942962287323f984a852e858a95a
lntechfuse <161242106+lntechfuse@users.noreply.github.com>
2024-09-04 18:16:28 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Patchanop Rodklueng"
+            studentId="660612151"
           />
         </div>
       </body>
